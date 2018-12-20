c99a98d799595735fdc3eed09c42c9a523a25848
abourke09 <aabourke@gmail.com>
2018-12-20 15:45:04 -0800

    adds Movie class constructor and prototype to fill in the movie-details div

--- app/assets/javascripts/movie.js
+++ app/assets/javascripts/movie.js
@@ -7,7 +7,36 @@ $(function (){
 function listenForMovieClick() {
   $('td a').on('click', function (event) {
 
+    $.ajax({
+      url: this.href,
+      method: 'get',
+      dataType: 'json',
+      success: function (response) {
+        console.log("response: ", response);
+
+        let movie = new Movie(response.data);
+        console.log("newMovieData: ", movie);
+
+        let html = movie.movieHTML()
+        $('div#movie-details').html(html)
+      }
+    })
+
     event.preventDefault();
 
   })
+}
+
+class Movie {
+  constructor(obj) {
+    this.id = obj.id
+    this.title = obj.attributes.title
+  }
+}
+
+Movie.prototype.movieHTML = function () {
+  return (`
+    <p>Id: ${this.id}</p>
+    <p>Title: ${this.title}</p>
+    `)
 }
