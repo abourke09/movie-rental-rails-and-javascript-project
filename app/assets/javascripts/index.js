11f96dd8535a6b88af383671fa4b179e3b2edfed
abourke09 <aabourke@gmail.com>
2019-01-08 10:22:14 -0800

    move JS logic to main index.js file, start building out functions to clear the welcome message div and whiteboard div (placeholder for all content to appear on)

---
+++ app/assets/javascripts/index.js
@@ -0,0 +1,72 @@
+$(function (){
+  console.log('Yay! index.js loaded.');
+  listenForMovieClick()
+
+})
+
+function clearWelcomeMessage() {
+  $('div#welcome-message').html('')
+}
+
+function clearWhiteboard() {
+  $('div#whiteboard').html('')
+}
+
+function listenForMovieClick() {
+  $('td a').on('click', function (event) {
+
+    $.ajax({
+      url: this.href,
+      method: 'get',
+      dataType: 'json',
+      success: function (response) {
+
+        let movie = new Movie(response);
+      //  console.log("This is a movie: ", movie);
+
+        let html = movie.movieHTML();
+        $('div#movie-details').html(html)
+
+        $('div#famous-quote-details').html(`<h2>Famous Quotes</h2>`)
+
+        movie.famous_quotes.forEach(q => {
+          let each_quote = `<p>"<em>${q.quote}</em>" - ${q.actor} </p> <br />`
+          $('div#famous-quote-details').append(each_quote)
+          })
+
+        let button = `<p>ISABUTTON!</p>`;
+
+        $('div#rent-movie-button').html(button)
+
+      }
+    })
+
+    event.preventDefault();
+
+  })
+}
+
+class Movie {
+  constructor(obj) {
+
+    this.id = obj.id
+    this.title = obj.title
+    this.lead_actor = obj.lead_actor
+    this.length = obj.length
+    this.rating = obj.rating
+    this.famous_quotes = obj.famous_quotes
+  }
+}
+
+Movie.prototype.movieHTML = function () {
+  return (`
+    <h1>${this.title}</h1>
+    <p><strong>MPAA Rating: </strong>${this.rating} </p>
+    <p><strong>Length: </strong>${this.length} minutes</p>
+    <p><strong>Lead Actor: </strong>${this.lead_actor} </p>
+
+    <form method="post" action="/rentals">
+      <input type="submit" id="${this.id}" value="Rent Movie">
+    </form>
+    `)
+}
