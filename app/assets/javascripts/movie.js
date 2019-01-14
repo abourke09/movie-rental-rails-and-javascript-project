092af6cbbfa8f28c64efec9c350f7d165a1fc5d1
abourke09 <aabourke@gmail.com>
2019-01-14 13:59:45 -0800

    comments out Rent button issues for now, uses .join to remove commas from famous quotes list

--- app/assets/javascripts/movie.js
+++ app/assets/javascripts/movie.js
@@ -20,20 +20,10 @@ Movie.prototype.movieHTML = function () {
     <p><strong>Length: </strong>${this.length} minutes</p>
     <p><strong>Lead Actor: </strong>${this.lead_actor} </p>
     <p><strong>Famous Quotes</strong></p>
-    <div>${movieQuotes}</div>
+    <div>${movieQuotes.join(' ')}</div>
     <button class="show_details" data-movie_id="${this.id}" value="Rent Movie">Rent</button>
     `)
 }
-
-//Movie.prototype.rentButton = function () {
-//  let movieId = this.id
-//  debugger
-//  return (`
-//    <form method="post" action="/rentals">
-//      <input type="submit" class="show_details" data-movie_id="${movieId}" value="Rent Movie">
-//    </form>
-//    `)
-//}
 
 Movie.prototype.movieList = function () {
   return (`
@@ -106,23 +96,27 @@ function listenForMovieClick() {
 function listenForRentClick() {
   $('button.show_details').on('click', function (event) {
     event.preventDefault()
-    event.stopPropagation()
 
-    let data = this.dataset.movie_id
+  //  url = 'http://localhost:3000/customers/8/rentals'
+  //  data = {
+  //      'authenticity_token': $("input[name='authenticity_token']").val(),
+  //      'food': {
+  //          'name': $("#food_name").val(),
+  //          'group_id': $("#food_group_id").val(),
+  //          'cals': $("#food_cals").val()
+  //      }
+  //  }
 
-/// Google: ajax post request data
-    $.ajax({
-      url: 'http://localhost:3000/customers/8/rentals',
-      data: {movie_id: data},
-      method: 'post',
-    //  dataType: 'json',
-      success: function (response) {
-        debugger
+  //  event.stopImmediatePropagation();
 
+  //  $.ajax({
+  //    type: 'POST',
+  //    url: url,
+  //    data: data,
+  //    success: function (response) {
 
-      }
-    })
-    console.log("Rent button click event:", event);
+  //    }
+  //  })
     //After movie is rented,  it should be added to the customer's rentals (in the database)
     //and the whiteboard should fill with the customer's Rentals info
     rentalsNavClick()
