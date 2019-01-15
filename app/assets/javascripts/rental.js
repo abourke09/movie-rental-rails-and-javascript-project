3465c6533bb3a1f1dd554e9dc53499baeb5f92da
abourke09 <aabourke@gmail.com>
2019-01-15 12:37:16 -0800

    builds out prototypes for both a checked out rental and a returned rental. Adds logic to AJAX request to send each type of rental to the appropriate prototype and then appends them to the correct table in the HTML.

--- app/assets/javascripts/rental.js
+++ app/assets/javascripts/rental.js
@@ -4,7 +4,35 @@ class Rental {
     this.customer_id = obj.customer_id
     this.movie_id = obj.movie_id
     this.status = obj.status
+    this.title = obj.movie_details.title
+    this.rating = obj.movie_details.rating
+    this.length = obj.movie_details.length
+    this.lead_actor = obj.movie_details.lead_actor
   }
+}
+
+Rental.prototype.CheckedOutTable = function () {
+  return (`
+        <tr>
+          <td><a href="movies/${this.movie_id}">${this.title}</a></td>
+          <td>${this.rating}</td>
+          <td>${this.length} minutes</td>
+          <td>${this.lead_actor}</td>
+          <td><button class="return" data-rental_id="${this.id}">Return Now</button></td>
+        </tr>
+  `)
+}
+
+Rental.prototype.ReturnedTable = function () {
+  return (`
+        <tr>
+          <td><a href="movies/${this.movie_id}">${this.title}</a></td>
+          <td>${this.rating}</td>
+          <td>${this.length} minutes</td>
+          <td>${this.lead_actor}</td>
+          <td><button class="add_quote" data-movie_id="${this.movie_id}">Add a Famous Quote</button></td>
+        </tr>
+  `)
 }
 
 function rentalsNavClick(event) {
@@ -15,47 +43,52 @@ function rentalsNavClick(event) {
 
   <h3>Currently Checked Out:</h3>
     <table>
-      <tr>
-        <th>Title</th>
-        <th>MPAA Rating</th>
-        <th>Length</th>
-        <th>Lead Actor</th>
-        <th>Return?</th>
-      </tr>
+      <tbody class="checked_out">
+        <tr>
+          <th>Title</th>
+          <th>MPAA Rating</th>
+          <th>Length</th>
+          <th>Lead Actor</th>
+          <th>Return?</th>
+        </tr>
+      </tbody>
     </table>
   <br></br>
 
   <h3>Past Rentals:</h3>
   <table>
-    <tr>
-      <th>Title</th>
-      <th>MPAA Rating</th>
-      <th>Length</th>
-      <th>Lead Actor</th>
-      <th>Add Quote?</th>
-    </tr>
+    <tbody class="returned">
+      <tr>
+        <th>Title</th>
+        <th>MPAA Rating</th>
+        <th>Length</th>
+        <th>Lead Actor</th>
+        <th>Add Quote?</th>
+      </tr>
+    </tbody>
   </table>
 
   <div id='movie-details'></div>
 
   `)
 
-
   $.ajax({
     url: event.delegateTarget.href,
     method: 'get',
     dataType: 'json',
     success: function (response) {
-console.log("Rentals json hopefully:", response)
+      let rentals = response["rentals"]
 
-      let checked_out = response["all_rentals"]["checked_out"]
-      let past_rentals = response["all_rentals"]["past_rentals"]
-
-  //    response.forEach(item => {
-  //      let newMovie = new Movie(item)
-  //      $('div#whiteboard tbody').append(newMovie.movieList())
-  //    })
-  //    listenForMovieClick()
+      rentals.forEach(function(element) {
+        if (element.status == "checked out") {
+          let newRental = new Rental(element)
+          $('div#whiteboard tbody.checked_out').append(newRental.CheckedOutTable())
+        } else if (element.status == "returned") {
+          let newRental = new Rental(element)
+          $('div#whiteboard tbody.returned').append(newRental.ReturnedTable())
+        }
+      })
+      listenForMovieClick()
     }
   })
 }
