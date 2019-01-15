8892dc152bd13d7a125fc7ad39049c23a66084aa
abourke09 <aabourke@gmail.com>
2019-01-15 10:52:50 -0800

    builds out signUpNavClick and listener for the signup button click event. Edits HTML form input types to be more specific (instead of just text -> number, email, password)

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -19,11 +19,37 @@ function homeNavClick(event) {
 }
 
 function logOutNavClick() {
-  $('div#whiteboard').append("you clicked on the Log Out link!")
+  $('div#whiteboard').append("This Nav click should always be a button that routes to sessions#destroy")
 }
 
 function signUpNavClick() {
-  $('div#whiteboard').append("you clicked on the Sign Up link!")
+  $('div#whiteboard').html(
+    `<h1>Please Sign Up</h1>
+    <form>
+      <br><strong>Name:</strong><br>
+      <input type="text" name="name"></br>
+
+      <br><strong>Age:</strong><br>
+      <input type="number" name="age"></br>
+
+      <br><strong>Email:</strong><br>
+      <input type="email" name="email"></br>
+
+      <br><strong>Password:</strong><br>
+      <input type="password" name="password"></br>
+
+      <br><input class="signup" type="submit" value="Create Customer">
+    </form>
+    `
+  )
+  listenForSignupClick(event)
+}
+
+function listenForSignupClick(event) {
+  event.preventDefault()
+  $('input.signup').on('click', function (event) {
+    console.log("Event from Create Customer button click:", event)
+  })
 }
 
 function logInNavClick(event) {
@@ -31,10 +57,10 @@ function logInNavClick(event) {
     `<h1>Please Log In</h1>
     <form>
       <br><strong>Email:</strong><br>
-      <input type="text" name="email"></br>
+      <input type="email" name="email"></br>
 
       <br><strong>Password:</strong><br>
-      <input type="text" name="password"></br>
+      <input type="password" name="password"></br>
 
       <br><input class="login" type="submit" value="Log In">
     </form>
