fde4478e306c64daff219a69d8dd681df0f1188d
abourke09 <aabourke@gmail.com>
2019-01-14 17:35:36 -0800

    removes mpaa_statement logic and replaces it with a call to the json obj.check_for_age

--- app/assets/javascripts/customer.js
+++ app/assets/javascripts/customer.js
@@ -4,32 +4,11 @@ class Customer {
     this.name = obj.name
     this.age = obj.age
     this.email = obj.email
+    this.check_for_age = obj.check_for_age
   }
 }
 
 Customer.prototype.profileHTML = function () {
-
-  let mpaa_statement = `According the the Motion Picture Association of America (MPAA),
-  you are old enough to see movies with a rating of `
-  let age = `${this.age}`
-  let under_thirteen = `G.`
-  let thirteen_to_seventeen = `G, PG, and PG-13.`
-  let eighteen_and_over = `G, PG, PG-13, R, and NC-17.`
-
-    if (age) {
-      if (age < 13) {
-      mpaa_statement += under_thirteen
-      } else if (age <= 17) {
-      mpaa_statement += thirteen_to_seventeen
-      } else {
-      mpaa_statement += eighteen_and_over
-      }
-    } else {
-      mpaa_statement = `The Motion Picture Association of America (MPAA) rates each movie
-      to recommend a certain age threshold for viewers. To learn more
-      about which movies would suit you, be sure to include your age
-      in your profile.`
-    }
 
   return (`
     <h1>${this.name}'s Profile Page </h1>
@@ -38,13 +17,33 @@ Customer.prototype.profileHTML = function () {
     <p>Age: ${this.age}</p>
 
     <div style="width:50%">
-      ${mpaa_statement}
+      ${this.check_for_age}
     </div>
-
-    <h3><%= link_to "Edit your profile", edit_customer_path(@customer.id) %></h3>
+    <br>
+    <button class="edit_profile" data-customer_id="${this.id}" value="Edit Profile">Edit Profile</button>
     `)
 }
 
+Customer.prototype.editProfile = function () {
+  return (`
+    <h1>Edit Your Profile</h1>
+    <form>
+      <br><strong>Name:</strong><br>
+      <input type="text" name="name" value="${this.name}"></br>
+
+      <br><strong>Age:</strong><br>
+      <input type="number" name="age" value="${this.age}"></br>
+
+      <br><strong>Email:</strong><br>
+      <input type="text" name="email" value="${this.email}"></br>
+
+      <br><strong>Password:</strong><br>
+      <input type="text" name="password"></br>
+
+      <br><input class="update_customer" type="submit" value="Update Customer">
+    </form>
+    `)
+}
 
 function profileNavClick(event) {
   $.ajax({
@@ -52,8 +51,30 @@ function profileNavClick(event) {
     method: 'get',
     dataType: 'json',
     success: function (response) {
-      let cust = new Customer(response)
-      $('div#whiteboard').append(cust.profileHTML())
+
+      let customer = new Customer(response)
+      $('div#whiteboard').append(customer.profileHTML())
+
+      listenForEditProfileClick(customer)
       }
     })
+}
+
+function listenForEditProfileClick(customer) {
+  let cust = customer
+
+  $('button.edit_profile').on('click', function (event) {
+    clearWhiteboard()
+    $('div#whiteboard').append(cust.editProfile())
+
+    listenForUpdateCustomerClick()
+  })
+}
+
+function listenForUpdateCustomerClick() {
+  event.preventDefault()
+  $('button.update_customer').on('click', function (event) {
+    debugger
+    console.log("Event from UpdateCustomerClick:", event)
+  })
 }
