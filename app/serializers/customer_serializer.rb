c2c50bf48e17eebe6e39b25f8ca4b17b642f0089
abourke09 <aabourke@gmail.com>
2019-01-29 14:55:02 -0800

    removes 'through' (not needed to describe relationship)

--- app/serializers/customer_serializer.rb
+++ app/serializers/customer_serializer.rb
@@ -2,7 +2,7 @@ class CustomerSerializer < ActiveModel::Serializer
   attributes :id, :name, :age, :email, :check_for_age
 
   has_many :rentals
-  has_many :movies, through: :rentals
+  has_many :movies
 
 
 end
--- app/serializers/movie_serializer.rb
+++ app/serializers/movie_serializer.rb
@@ -2,6 +2,6 @@ class MovieSerializer < ActiveModel::Serializer
   attributes :id, :title, :rating, :length, :lead_actor
 
   has_many :rentals
-  has_many :customers, through: :rentals
+  has_many :customers
   has_many :famous_quotes
 end
