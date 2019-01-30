034d120afd28392182257e41d4404f85a22fc931
abourke09 <aabourke@gmail.com>
2019-01-30 11:29:27 -0800

    Edits sorting function and introduces sorting for movie title (id#), length, and lead actor.

--- app/assets/javascripts/movie.js
+++ app/assets/javascripts/movie.js
@@ -53,30 +53,13 @@ function clearFilter() {
 		$('.content').show();
 }
 
-function sortTable(column) {
-  let table = $('div.column.left table')
-  let rows = $('div.column.left tr.content')
-
-  switching = true ;
-  dir = "asc";
-  while (switching) {
-    switching = false
-    for (i = 1; i < (rows.length - 1); i++) {
-
-    }
-  }
-
-// https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_sort_table_desc
-
-}
-
 function moviesNavClick() {
   $('div.column.left').html(`
     <h1>All Movies:</h1>
     <table class="table table-bordered">
       <tbody>
         <tr>
-          <th onclick="sortTable("title")">Title</th>
+          <th onclick="sortByHeaders(0)">Title</th>
           <th>MPAA Rating
             <select id='filterText' style='display:inline-block' onchange='filterText()'>
               <option disabled selected>Select</option>
@@ -88,8 +71,8 @@ function moviesNavClick() {
               <option value='all'>All</option>
             </select>
           </th>
-          <th onclick="sortTable("length")">Length</th>
-          <th onclick="sortTable("lead actor")">Lead Actor</th>
+          <th onclick="sortByHeaders(2)">Length</th>
+          <th onclick="sortByHeaders(3)">Lead Actor</th>
         </tr>
       </tbody>
     </table>
@@ -107,6 +90,53 @@ function moviesNavClick() {
       listenForMovieClick()
     }
   })
+}
+
+function sortByHeaders(n) {
+  var rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+  switching = true;
+  dir = "asc";
+
+  while (switching) {
+    switching = false;
+    rows = $('div.column.left tr.content');
+    for (i = 0; i < (rows.length - 1); i++) {
+      shouldSwitch = false;
+      x = rows[i].getElementsByTagName("TD")[n];
+      y = rows[i + 1].getElementsByTagName("TD")[n];
+
+      if (n == 3) {
+        thingOne = x.innerText
+        thingTwo = y.innerText
+      } else {
+        thingOne = parseInt(x.innerText)
+        thingTwo = parseInt(y.innerText)
+      }
+      
+      if (dir == "asc") {
+        if (thingOne > thingTwo) {
+          shouldSwitch= true;
+          break;
+        }
+      } else if (dir == "desc") {
+        if (thingOne < thingTwo) {
+          shouldSwitch = true;
+          break;
+        }
+      }
+    }
+    if (shouldSwitch) {
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      switching = true;
+      switchcount ++;
+    } else {
+      if (switchcount == 0 && dir == "asc") {
+        dir = "desc";
+        switching = true;
+      }
+    }
+  }
+
 }
 
 function listenForMovieClick() {
